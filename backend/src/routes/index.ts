71d44fd4eacb15bf0529b502e8e22473cc67efb2
0xbyt3z <rtheekshanafernando@gmail.com>
2025-09-18 21:38:46 +0530

    chore(api): changes to routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Application } from "express";
-import homeRoutes from "./home.routes";
-import tutorialRoutes from "./tutorial.routes";
+import appRoutes from "./app.routes";
 
 export default class Routes {
   constructor(app: Application) {
-    app.use("/api", homeRoutes);
-    app.use("/api/tutorials", tutorialRoutes);
+    app.use("/api", appRoutes);
   }
 }
